db07388991ae285a3b91625576f6f7c56d968c58
sanjay <Bhavishm009@gmail.com>
2024-02-07 01:27:21 +0530

    changes in some files

--- api/account.js
+++ api/account.js
@@ -3,40 +3,46 @@ const resp = require('../helpers/response');
 const con = require('../constants/index');
 
 const dummyUsers = [
-    {
-      id: 1,
-      username: 'john_doe',
-      email: 'john.doe@example.com',
-      age: 25,
-    },
-    {
-      id: 2,
-      username: 'jane_smith',
-      email: 'jane.smith@example.com',
-      age: 30,
-    },
-    {
-      id: 3,
-      username: 'bob_jackson',
-      email: 'bob.jackson@example.com',
-      age: 28,
-    },
-    // Add more dummy users as needed
-  ];
+  {
+    id: 1,
+    username: 'john_doe',
+    email: 'john.doe@example.com',
+    age: 25,
+  },
+  {
+    id: 2,
+    username: 'jane_smith',
+    email: 'jane.smith@example.com',
+    age: 30,
+  },
+  {
+    id: 3,
+    username: 'bob_jackson',
+    email: 'bob.jackson@example.com',
+    age: 28,
+  },
+  {
+    id: 4,
+    username: 'Bob jackson',
+    email: 'bob.jackson@example.com',
+    age: 28,
+  },
+
+];
 
 const account = {
-    login: asyncHandler(async(req, res) => {
-        const body = req.body;
-        console.log(body);
-        return res.json({
-            message: "Hello"
-        })
-    }),
-    users: asyncHandler(async(req, res) => {
-        const body = req.body;
-        console.log(body);
-        return resp.cResponse(req, res, resp.SUCCESS, con.account.RECORD_SUCCESS,dummyUsers );
+  login: asyncHandler(async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.json({
+      message: "Hello"
     })
+  }),
+  users: asyncHandler(async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return resp.cResponse(req, res, resp.SUCCESS, con.account.RECORD_SUCCESS, dummyUsers);
+  })
 }
 
 module.exports = account;
